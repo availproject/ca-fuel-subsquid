8063302394009a07e81a78faf9727a5a2efaf1c4
mmjee <root@git.maharshi.ninja>
2025-05-30 19:02:42 +0530

    Initial commit

---
+++ db/migrations/1748339516233-Data.js
@@ -0,0 +1,11 @@
+module.exports = class Data1748339516233 {
+    name = 'Data1748339516233'
+
+    async up(db) {
+        await db.query(`CREATE TABLE "log_entry" ("id" character varying NOT NULL, "found_at" integer NOT NULL, "contract" bytea NOT NULL, "rb" numeric NOT NULL, "data" bytea NOT NULL, CONSTRAINT "PK_45e2f8fa5e70dd60266d2f94d49" PRIMARY KEY ("id"))`)
+    }
+
+    async down(db) {
+        await db.query(`DROP TABLE "log_entry"`)
+    }
+}
